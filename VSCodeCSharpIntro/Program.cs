257e3494f6a03b94c6b71f01d3da47f0708492ee
Michael Wynnychuk <m.wynnychuk@uleth.ca>
2021-08-01 16:21:55 -0600

    Updating Program and Program2

--- VSCodeCSharpIntro/Program.cs
+++ VSCodeCSharpIntro/Program.cs
@@ -5,32 +5,41 @@ namespace C_Sharp_Project // Namespace is a collection of classes
     class Rectangle // this is a class declaration
     {
 
-        public double length;
-        public double width;
+        public double length =0;
+        public double width=0;
 
         public int helloWorld1(int width, int length)
         {
             return width* length;
         }
+        /*
         public void setWidth(int w){
             width = w; // updates the class variable width value
         }
         public void setLength(int l){
             length = l; // updates the class variable length value
         }
+        */
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Rectangle r = new Rectangle();
-            int area =0;
+            //Rectangle r = new Rectangle();
+            Rectangle2 r2 = new Rectangle2();
+            double area =0, area2=0;
             Console.WriteLine("Hello World!");
-            area = r.helloWorld1(5, 5);
-            r.setLength(3);
-            r.setWidth(3);
-            Console.WriteLine("The area is " + area);
-            Console.WriteLine(r.width);
+            area2 = r2.helloWorld1(5, 5);
+            //area = r.helloWorld1(5, 5);
+
+            r2.setLength(5);
+            r2.setWidth(4);
+
+            area2 = r2.width2*r2.length2;
+           // r.setLength(3);
+            //r.setWidth(3);
+            Console.WriteLine("The area is " + area2);
+            Console.WriteLine(r2.width2);
             //Console.ReadKey();
         }
     }
--- VSCodeCSharpIntro/Program2.cs
+++ VSCodeCSharpIntro/Program2.cs
@@ -2,35 +2,20 @@ using System;
 
 namespace C_Sharp_Project // Namespace is a collection of classes
 {
-    class Rectangle // this is a class declaration
+    class Rectangle2
     {
-        public double length;
-        public double width;
+        public double length2;
+        public double width2;
 
-        public int helloWorld1(int width, int length)
+        public int helloWorld1(int width2, int length2)
         {
-            return width* length;
+            return width2* length2;
         }
         public void setWidth(int w){
-            width = w; // updates the class variable width value
+            width2 = w; // updates the class variable width value
         }
         public void setLength(int l){
-            length = l; // updates the class variable length value
-        }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Rectangle r = new Rectangle();
-            int area =0;
-            Console.WriteLine("Hello World!");
-            area = r.helloWorld1(5, 5);
-            r.setLength(3);
-            r.setWidth(3);
-            Console.WriteLine("The area is " + area);
-            Console.WriteLine(r.width);
-            //Console.ReadKey();
+            length2 = l; // updates the class variable length value
         }
     }
 }
